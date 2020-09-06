eec6b5f73d1fc6e4db6405e9366fa56d607ccabb
lukaskurnia <lukasjonathan99@gmail.com>
2020-09-06 16:13:57 +0700

    finish decryption and update encrypt for super encryption

--- src/algorithm/const.py
+++ src/algorithm/const.py
@@ -1,3 +1,5 @@
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 LETTER = 97
 CAPITAL = 65
+SENTINEL = 224
+TRANSPOSEKEY = 4
--- src/algorithm/super.py
+++ src/algorithm/super.py
@@ -4,13 +4,20 @@ from src.algorithm.vigenere import Vigenere
 import math
 import numpy as np
 
+#Create a matrices from a number sequence with max col = length
+# example: [4, 7, 13, 3, 1, 22, 11, 224, 18, 20, 15, 224, 18, 18, 17, 224]
+# return : [[4, 7, 13, 3], 
+#           [1, 22, 11, 224],
+#           [18, 20, 15, 224],
+#           [18, 18, 17, 224]]
+
 def split_len(seq, length):
     matrices = [seq[i:i + length] for i in range(0, len(seq), length)]
     nRow = math.ceil(len(seq)/length)
     lengthLastRow = len(matrices[nRow-1])
     if(lengthLastRow != length):
         for i in range(0,(length-lengthLastRow)):
-            matrices[nRow-1].append(-1)
+            matrices[nRow-1].append(const.SENTINEL)
     return matrices
 
 class SuperEncryption:
@@ -20,9 +27,7 @@ class SuperEncryption:
 
     def preprocess(self):
         # Preprocess text
-        text = self.text.lower()
-        text = general.sanitize(text)
-        self.text = [(ord(i)-const.LETTER) for i in text]
+        # Done in encrypt and decrypt function for special purpose
                 
         # Preprocess key
         key = self.key.lower()
@@ -31,26 +36,50 @@ class SuperEncryption:
 
     def encrypt(self):
         chiper_text = ''
+        
+        #Preprocess text
+        text = self.text.lower()
+        text = general.sanitize(text)
+        self.text = [(ord(i)-const.LETTER) for i in text]
+
+        #Encrypt phase
         vig = Vigenere(self.text,self.key).encrypt()
+
         ord_vig = [(ord(i)-const.LETTER) for i in vig]
-        matrices = split_len(ord_vig,4)
-        print(matrices)
+        matrices = split_len(ord_vig,const.TRANSPOSEKEY)
         transpose = np.transpose(matrices)
-        print(transpose)
+
         for row in transpose:
             for col in row:
-                if(col != -1):
+                if(col != const.SENTINEL):                
                     chiper_text += general.order_to_char(col)
+                else:
+                    chiper_text += chr(col)
 
         return chiper_text
   
     def decrypt(self):
         chiper_text = ''
-        idx = 0
-        for char in self.text:
-            if(idx >= len(self.key)):
-                idx = 0
-            chiper_text += chr(((char - self.key[idx]) % 26)+const.LETTER)
-            idx += 1
 
-        return chiper_text
+        #Preprocess text
+        text = []        
+        for i in self.text:
+            if(i != chr(const.SENTINEL)):
+                text.append(ord(i)-const.LETTER)
+            else:
+                text.append(const.SENTINEL)
+
+        #Decrypt phase
+        nRow = int(len(text)/const.TRANSPOSEKEY)
+        matrices = split_len(text,nRow)
+        transpose = np.transpose(matrices)
+
+        for row in transpose:
+            for col in row:
+                if(col != const.SENTINEL):                
+                    chiper_text += general.order_to_char(col)
+
+        chiper_text = [(ord(i)-const.LETTER) for i in chiper_text]
+        vig = Vigenere(chiper_text,self.key).decrypt()        
+
+        return vig
